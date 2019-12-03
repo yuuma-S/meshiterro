915659b3cd5f1a9561578cb6ad9905bd9727e2db
sakurai-Y <yuma@sakurai-YnoMacBook-Pro.local>
2019-12-03 15:13:38 +0900

    meshiterro check_point1

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,11 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '0733dd85f26578b90a102550775274e3a1b11f411207a557965ed61ed94a63818a7f712b4ba79be71058cda398ac9b86ccc793c24ef832973719b4bb3683aeae'
+
---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+
+  devise_for :users
+  root 'post_images#index'
+
+  resources :post_images, only:[:new, :create, :index, :show, :destroy] do
+  	resource :post_comments, only: [:create, :destroy]
+  	resource :favorites, only: [:create, :destroy]
+  end
+
+  resources :users, only:[:show]
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
